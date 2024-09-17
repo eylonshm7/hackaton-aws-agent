c3394fb35d4090a0484e4e85179d001c46b77a65
eylonshm7 <eshmilovich@go7.io>
2024-09-17 20:32:00 +0300

    fix aws config error

--- callAgent.js
+++ callAgent.js
@@ -3,17 +3,17 @@ import {
   InvokeAgentCommand,
 } from "@aws-sdk/client-bedrock-agent-runtime";
 
+const config = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+};
+
 const client = new BedrockAgentRuntimeClient({
   region: "us-east-1",
   credentials: config,
 });
 
 export const invokeAgent = async function ({ sessionId, inputText } = {}) {
-  const config = {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  };
-
   let completion = "";
   const input = {
     agentId: "BDDTKWNTKO",
